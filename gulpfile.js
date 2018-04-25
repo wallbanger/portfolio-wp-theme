930aaf65fb1c49f214dfca2fbdf5e271b045ea6c
Eugene Koval <iam.eugenekoval@gmail.com>
2018-04-25 16:04:02 +0300

    rm comments

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     plumber = require('gulp-plumber');
 
-/* Wait for jekyll-build, then launch the Server */
 gulp.task('browser-sync', ['sass'], function() {
   browserSync({
     server: {
@@ -37,7 +36,6 @@ gulp.task('jsMin', ['jsConcat'], function() {
       .pipe(gulp.dest('assets/js'));
 });
 
-/* Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds) */
 gulp.task('sass', function () {
   return gulp.src([
     '_source/scss/main.scss'
@@ -57,7 +55,6 @@ gulp.task('sass', function () {
       .pipe(gulp.dest('assets/css'));
 });
 
-/* Watch scss files for changes & recompile, watch html/md files, run jekyll & reload BrowserSync */
 gulp.task('watch', ['jsMin', 'browser-sync'], function () {
   gulp.watch([
     '_source/**/*.scss'
@@ -69,5 +66,4 @@ gulp.task('watch', ['jsMin', 'browser-sync'], function () {
   ], ['jsMin']);
 });
 
-/* Default task, running just `gulp` will compile the sass, compile the jekyll site, launch BrowserSync & watch files. */
 gulp.task('default', ['sass', 'jsMin']);
